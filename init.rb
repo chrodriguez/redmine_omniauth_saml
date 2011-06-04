14ee3a5ccedfb450398e0c6e6021f06d080c499e
jbbarth <jeanbaptiste.barth@gmail.com>
2011-06-05 01:40:27 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_omniauth do
+  name 'Redmine Omniauth plugin'
+  description 'This plugin adds Omniauth (https://github.com/intridea/omniauth) support to Redmine'
+  author 'Jean-Baptiste BARTH'
+  author_url 'mailto:jeanbaptiste.barth@gmail.com'
+  url 'http://github.com/jbbarth/redmine_omniauth'
+  version '0.0.1'
+end
