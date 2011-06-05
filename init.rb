b61dc3347cda98214b2d2d4a63ecb98d0a5d22ff
jbbarth <jeanbaptiste.barth@gmail.com>
2011-06-05 22:04:27 +0200

    Configure OmniAuth 'full_host' in case the application runs behing a reverse-proxy

--- init.rb
+++ init.rb
@@ -22,6 +22,21 @@ config.to_prepare do
   require_dependency 'redmine_omniauth_cas/account_controller_patch'
 end
 
+# Full host in case the apps runs behind a reverse-proxy
+OmniAuth.config.full_host = Proc.new do |env|
+  url = env["omniauth.origin"] || env["rack.session"]["omniauth.origin"]
+  #parse url from env and remove both request_uri and query_string
+  if url.present?
+    uri = URI.parse(url)
+    url = "#{uri.scheme}://#{uri.host}"
+    url << ":#{uri.port}" unless uri.default_port == uri.port
+  #if no url found, fall back to config/app_config.yml addresses
+  else
+    url = Setting["host_name"]
+  end
+  url
+end
+
 # PROVIDERS
 
 # Sample CAS provider
