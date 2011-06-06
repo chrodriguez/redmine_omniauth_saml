fb6c9897a078ee6cf73366f8e8b14cf7986a5b6b
jbbarth <jeanbaptiste.barth@gmail.com>
2011-06-06 08:29:33 +0200

    Code cleanup : everything under Redmine::OmniAuthCAS module

--- lib/redmine_omniauth_cas/account_controller_patch.rb
+++ lib/redmine_omniauth_cas/account_controller_patch.rb
@@ -1,22 +1,25 @@
 require_dependency 'account_controller'
 
-class AccountController
-  def login_with_omniauth
-    auth = request.env["omniauth.auth"]
-    #user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
-    user = User.find_by_login(auth["uid"]) || User.find_by_mail(auth["uid"])
+module Redmine::OmniAuthCAS
+  module AccountControllerPatch
+    def login_with_omniauth
+      auth = request.env["omniauth.auth"]
+      #user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
+      user = User.find_by_login(auth["uid"]) || User.find_by_mail(auth["uid"])
 
-    # taken from original AccountController
-    # maybe it should be splitted in core
-    if user.blank?
-      invalid_credentials
-      redirect_to signin_url
-    else
-      successful_authentication(user)
+      # taken from original AccountController
+      # maybe it should be splitted in core
+      if user.blank?
+        invalid_credentials
+        redirect_to signin_url
+      else
+        successful_authentication(user)
+      end
+    end
+
+    def blank
+      render :text => "Not Found", :status => 404
     end
   end
-
-  def blank
-    render :text => "Not Found", :status => 404
-  end
 end
+AccountController.send(:include, Redmine::OmniAuthCAS::AccountControllerPatch)
--- lib/redmine_omniauth_cas/hooks.rb
+++ lib/redmine_omniauth_cas/hooks.rb
@@ -1,4 +1,4 @@
-module RedmineOmniauth
+module Redmine::OmniAuthCAS
   class Hooks < Redmine::Hook::ViewListener
     render_on :view_account_login_top, :partial => 'redmine_omniauth_cas/view_account_login_top'
   end
