3b46c5ab7ea1f1d6eb1ad2f55d5afc0e8a66cc59
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-23 00:11:23 +0200

    Added a new patch to OmniAuth::CAS so that it behaves well when the serviceValidate url has a different host

--- lib/omniauth/patches.rb
+++ lib/omniauth/patches.rb
@@ -1,15 +1,28 @@
 require 'omniauth/cas'
 
-# patch to accept path (subdir) in cas_host
 module OmniAuth
   module Strategies
     class CAS
+      # patch to accept path (subdir) in cas_host
       option :path, nil
 
       def cas_host_with_path
         @cas_host ||= cas_host_without_path + @options.path.to_s
       end
       alias_method_chain :cas_host, :path
+
+      # patch to accept a different host for service_validate_url
+      def service_validate_url_with_different_host(service_url, ticket)
+        service_url = Addressable::URI.parse( service_url )
+        service_url.query_values = service_url.query_values.tap { |qs| qs.delete('ticket') }
+
+        if service_url.host.nil?
+          cas_host + append_params(@options.service_validate_url, { :service => service_url.to_s, :ticket => ticket })
+        else
+          append_params(@options.service_validate_url, { :service => service_url.to_s, :ticket => ticket })
+        end
+      end
+      alias_method_chain :service_validate_url, :different_host
     end
   end
 end
