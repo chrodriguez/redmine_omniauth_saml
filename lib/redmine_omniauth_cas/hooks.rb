0b21f043826afad49a63b31d947534408cd6f259
Christian Rodriguez <car@cespi.unlp.edu.ar>
2013-09-22 16:49:49 -0300

    remove unnecesary patches

--- lib/redmine_omniauth_cas/hooks.rb
+++
@@ -1,5 +0,0 @@
-module Redmine::OmniAuthCAS
-  class Hooks < Redmine::Hook::ViewListener
-    render_on :view_account_login_top, :partial => 'redmine_omniauth_cas/view_account_login_top'
-  end
-end
