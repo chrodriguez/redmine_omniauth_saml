9a4b4ec0f2e9928498652b4fc5d2daaf944a96e9
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-09-24 03:50:19 -0300

    Check if user with omniauth email exists
    When a user try to access redmine from saml we try to create it if ti
    doesn't exists a user with specified login name. That was not enough
    because, there may be a user with other login name in redmine but with
    the same email that comes from saml. So we try to find that user and
    return it as logged in user

--- lib/redmine_omniauth_saml/user_patch.rb
+++ lib/redmine_omniauth_saml/user_patch.rb
@@ -5,7 +5,8 @@ class User
     user_attributes = Redmine::OmniAuthSAML.user_attributes_from_saml omniauth
     user = self.find_by_login(user_attributes[:login])
     unless user
-      if Redmine::OmniAuthSAML.onthefly_creation? 
+      user = EmailAddress.find_by(address: user_attributes[:mail]).try(:user)
+      if user.nil? && Redmine::OmniAuthSAML.onthefly_creation? 
         user = new user_attributes
         user.login    = user_attributes[:login]
         user.language = Setting.default_language
