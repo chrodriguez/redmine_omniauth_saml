bf6b63fb1f3e3e479da3201c6bbf58d2c2df0f15
Eskil Varenius <eskil.varenius@gmail.com>
2020-04-13 09:37:36 +0200

    Allow SSO attributes including dots

--- sample-saml-initializers.rb
+++ sample-saml-initializers.rb
@@ -13,10 +13,15 @@ Redmine::OmniAuthSAML::Base.configure do |config|
     :name_identifier_value          => "mail", # Which redmine field is used as name_identifier_value for SAML logout
     :attribute_mapping              => {
     # How will we map attributes from SSO to redmine attributes
-      :login      => 'extra.raw_info.username',
-      :mail       => 'extra.raw_info.email',
-      :firstname  => 'extra.raw_info.firstname',
-      :lastname   => 'extra.raw_info.firstname'
+    # using either urn:oid:identifier, or friendly names, e.g.
+    # :mail       => 'extra|raw_info|urn:oid:0.9.2342.19200300.100.1.3'
+    # or
+    # :mail       => 'extra|raw_info|email'
+    # Edit defaults below to match your attributes
+      :login      => 'extra|raw_info|username',
+      :mail       => 'extra|raw_info|email',
+      :firstname  => 'extra|raw_info|firstname',
+      :lastname   => 'extra|raw_info|firstname'
     }
   }
 
